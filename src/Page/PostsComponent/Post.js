e6bc9be45192c16f88a712254ceac07b43dc6b1b
Sreeraksha <raksharamesh1998@gmail.com>
2023-11-10 21:57:49 +0530

    styling blog project

--- src/Page/PostsComponent/Post.js
+++ src/Page/PostsComponent/Post.js
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from "react";
-import PropTypes from 'prop-types';
 
 const Post = props => {
     const [body, setBody] = useState({})
     const [userData, setUserData] = useState({})
+    const [randomDates, setRandomDates] = useState()
 
     const idFromURL = new URLSearchParams(window.location.search).get("id")
 
     const getBodyFromURL = () => {
         fetch('https://jsonplaceholder.typicode.com/posts/' + idFromURL).then(res => res.json()).then((jsonResponseData) => {
-            console.log('post id', jsonResponseData)
             setBody(jsonResponseData)
         })
     }
@@ -17,7 +16,6 @@ const Post = props => {
     const getUserFromURL = () => {
         if (body.userId) {
             fetch('https://dummyjson.com/users/' + body.userId).then(res => res.json()).then((jsonResponseData) => {
-                console.log('user id', jsonResponseData)
                 setUserData(jsonResponseData)
             })
         }
@@ -32,17 +30,68 @@ const Post = props => {
         getUserFromURL()
     }, [body])
 
+    const RandomDateGenerator = () => {
+        const getRandomInt = (min, max) => {
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+        };
+      
+        const getRandomDate = () => {
+          const year = getRandomInt(1970, 2023); // You can adjust the range of years as needed
+          const month = getRandomInt(1, 12);
+          const day = getRandomInt(1, 28); // Adjust the maximum day based on the month
+      
+          return setRandomDates( new Date(year, month - 1, day)); // Month in JavaScript Date object is zero-based
+        };
+    }
+
     return (
-        <div>
-            <h2>Hello world....Post id = {idFromURL}</h2>
-            <div>
-                <div>Title: {body.title}</div>
-                <div>Body: {body.body}</div>
-                <div>User_Name: {userData.firstName} {userData.lastName}</div>
-                <div>User_id: {userData.id}</div>
+        <div class="bg-white py-24 sm:py-32">
+            <div class="mx-auto max-w-7xl px-6 lg:px-8">
+                <div class="mx-auto max-w-2xl lg:mx-0">
+                    <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
+                    <p class="mt-2 text-lg leading-8 text-gray-600">Post id = {idFromURL}</p>
+                </div>
+
+                <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                    <article class="flex max-w-xl flex-col items-start justify-between">
+                        <div class="flex items-center gap-x-4 text-xs">
+                            <time dateTime="2020-03-16" class="text-gray-500">{randomDates}</time>
+                            <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">{userData.department}</a>
+                        </div>
+                        <div class="group relative">
+                            <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                                <a href="#">
+                                    <span class="absolute inset-0"></span>
+                                    {body.title}
+                                </a>
+                            </h3>
+                            <p class="mt-5 line-clamp-10 text-sm leading-6 text-gray-600">{body.body}</p>
+                        </div>
+                        <div class="relative mt-8 flex items-center gap-x-4">
+                            <img src={userData.image} alt="" class="h-10 w-10 rounded-full bg-gray-50" />
+
+                                <div class="text-sm leading-6">
+                                    <p class="font-semibold text-gray-900">
+                                        <a href="#">
+                                            <span class="absolute inset-0"></span>
+                                            {userData.firstName} {userData.lastName}
+                                        </a>
+                                    </p>
+                                    <p class="text-gray-600">{userData.title}</p>
+                                </div>
+                        </div>
+                    </article>
+
+                </div>
+
+                {/* <h2>Hello world....Post id = {idFromURL}</h2>
+                <div>
+                    <div>Body: {body.body}</div>
+                    <div>User_Name: {userData.firstName} {userData.lastName}</div>
+                    <div>User_id: {userData.id}</div>
+                </div> */}
             </div>
         </div>
-
     )
 };
 
--- src/Page/PostsComponent/index.js
+++ src/Page/PostsComponent/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { json } from "react-router-dom";
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const PostsComponent = () => {
 
@@ -11,26 +10,31 @@ const PostsComponent = () => {
      * method #1: async await 
      */
     const fetchPost = async () => {
-        // console.log('fetchpost')
         const dataFromPost = await fetch('https://jsonplaceholder.typicode.com/posts')
-        // console.log('promise from post api',dataFromPost)
         const jsonData = await dataFromPost.json()
-        // console.log('jsonData', jsonData)
         setPosts(jsonData)
     }
 
     useEffect(() => {
-        // console.log('useeffect')
         fetchPost().then()
-        // fetchUser()
     }, [])
 
     return (
-        <div>
-            <h2>Posts List</h2>
-            <div>
-                {posts.map((post) => <div><Link className="item" to={"/posts/post?id="+post.id}>{post.title}</Link>
-            </div>)}</div>
+        <div >
+            <h2 className="mb-4 p-8 text-4xl grid justify-items-center font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-4xl dark:text-white">Posts List</h2>
+            <div class="grid grid-cols-3 ">
+                {posts.map((post) =>
+                    <div className="flex flex-col p-8 m-8 items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                        <Link  to={"/posts/post?id=" + post.id}>
+                                <div class="flex flex-col justify-between p-4 leading-normal">
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{post.title}</h5>
+                                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Post id = {post.id}</p>
+                                </div>
+                        </Link>
+                        {/* <Link className="item" to={"/posts/post?id=" + post.id}>{post.title}</Link> */}
+                    </div>
+                )}
+            </div>
 
         </div>
     )
